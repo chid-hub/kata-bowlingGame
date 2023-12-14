593f5225dc6ba42d1af87600831fe63c20ede1c3
Chidhambaram S <csivaraman.c@nwc.com.sa>
2023-12-14 16:53:40 +0530

    initial project created

---
+++ BowlingGameKATA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BowlingGameKATA
+//
+//  Created by Chidhambaram S on 14/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
