a47c04c8f121a972eb43be61b6f98b67f79b21fb
Chidhambaram S <csivaraman.c@nwc.com.sa>
2023-12-14 18:28:56 +0530

    calculating score business logic updated

--- BowlingGameKATA/Model/BowlingGameModel.swift
+++ BowlingGameKATA/Model/BowlingGameModel.swift
@@ -10,12 +10,48 @@ import Foundation
 class BowlingGameModel{
     
     private var rolls = [Int]()
-
+    
     func roll(_ pins: Int) {
         rolls.append(pins)
     }
-
+    
     func score() -> Int {
-        return 0
+        var total = 0
+        var frameIndex = 0
+        
+        for _ in 0..<10 {
+            if isStrike(frameIndex) {
+                total += 10 + strikeBonus(frameIndex)
+                frameIndex += 1
+            } else if isSpare(frameIndex) {
+                total += 10 + spareBonus(frameIndex)
+                frameIndex += 2
+            } else {
+                total += openFrameScore(frameIndex)
+                frameIndex += 2
+            }
+        }
+        return total
     }
+    
+    private func isStrike(_ frameIndex: Int) -> Bool {
+        return rolls[frameIndex] == 10
+    }
+    
+    private func isSpare(_ frameIndex: Int) -> Bool {
+        return openFrameScore(frameIndex) == 10
+    }
+    
+    private func strikeBonus(_ frameIndex: Int) -> Int {
+        return rolls[frameIndex + 1] + rolls[frameIndex + 2]
+    }
+    
+    private func spareBonus(_ frameIndex: Int) -> Int {
+        return rolls[frameIndex + 2]
+    }
+    
+    private func openFrameScore(_ frameIndex: Int) -> Int {
+        return rolls[frameIndex] + rolls[frameIndex + 1]
+    }
+    
 }
